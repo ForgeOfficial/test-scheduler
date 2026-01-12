ad001f3d111ccec0ffe134f7ffe7d64997644056
Maxime <maxime.caville@next-u.fr>
2026-01-12 14:07:32 +0100

    feat(red): setTask edit if already exist

--- test/scheduler.spec.ts
+++ test/scheduler.spec.ts
@@ -13,5 +13,14 @@ describe(Scheduler.name, () => {
             expect(scheduler.tasks.length).toEqual(1);
             expect(scheduler.tasks[0].name).toEqual("task1");
         });
+
+        it('should edit task if name already exist', () => {
+            const scheduler = new Scheduler();
+            scheduler.setTask("task1", null, () => {});
+            scheduler.setTask("task1", undefined, () => {});
+            expect(scheduler.tasks.length).toEqual(1);
+            expect(scheduler.tasks[0].name).toEqual("task1");
+            expect(scheduler.tasks[0].period).toEqual(undefined);
+        });
     })
 });
