a498c5c9ef1d6e0d56a47afd950c22d6e6083fc2
Maxime <maxime.caville@next-u.fr>
2026-01-12 12:29:02 +0100

    init repo

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import("jest").Config} **/
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/**/*.spec.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_modules'],
+  reporters: ['default'],
+  globals: { 'ts-jest': { diagnostics: false } },
+  transform: {},
+};
