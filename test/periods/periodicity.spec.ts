888ac14df43fd6d7dd38d3f59505747a81cc5791
Maxime <maxime.caville@next-u.fr>
2026-01-12 14:40:24 +0100

    feat(refactor): impl periodicity

--- test/periods/periodicity.spec.ts
+++ test/periods/periodicity.spec.ts
@@ -1,11 +1,15 @@
 import {Always, Never} from "../../src/periods/Periodicity";
 
 describe('Periodicity', () => {
-    it('should always run', () => {
-        expect(new Always().shouldRun(new Date())).toEqual(true);
-    });
+    describe(Always.name, () => {
+        it('should always run', () => {
+            expect(new Always().shouldRun(new Date())).toEqual(true);
+        });
+    })
 
-    it('should never run', () => {
-        expect(new Never().shouldRun(new Date())).toEqual(false);
-    });
+    describe(Never.name, () => {
+        it('should never run', () => {
+            expect(new Never().shouldRun(new Date())).toEqual(false);
+        });
+    })
 });
