14a914fdb5a5c7c42adf8619450dab736129fabc
Maxime <maxime.caville@next-u.fr>
2026-01-12 14:52:28 +0100

    feat(green): add everyMinute / EveryHourAt Periodicity

--- src/periods/Periodicity.ts
+++ src/periods/Periodicity.ts
@@ -13,3 +13,31 @@ export class Never implements IPeriodicity {
         return false;
     }
 }
+
+export class EveryMinute implements IPeriodicity {
+    private lastRun: Date = new Date();
+    shouldRun(now: Date): boolean {
+        const currentMinute = now.getMinutes();
+        const lastMinuteRun = this.lastRun.getMinutes();
+        if (lastMinuteRun !== currentMinute) {
+            this.lastRun = now;
+            return true;
+        }
+        return false;
+    }
+}
+
+export class EveryHourAt implements IPeriodicity {
+    private lastRun: Date = new Date();
+    constructor(private minute: number) {}
+
+    shouldRun(now: Date): boolean {
+        const hm = `${now.getHours()}:${now.getMinutes()}`;
+        const lastRunHourMinute = `${this.lastRun.getHours()}:${this.lastRun.getMinutes()}`;
+        if (now.getMinutes() === this.minute && lastRunHourMinute !== hm) {
+            this.lastRun = now;
+            return true;
+        }
+        return false;
+    }
+}
