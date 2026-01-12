e90f3df6afc6ad19037089ec36fe9a0af696bef2
Maxime <maxime.caville@next-u.fr>
2026-01-12 14:13:50 +0100

    feat(refactor): setTask edit if already exist

--- src/Scheduler.ts
+++ src/Scheduler.ts
@@ -1,5 +1,10 @@
+interface Task {
+    period: any;
+    action: () => void;
+}
+
 export class Scheduler {
-    private readonly _tasks: Map<string, any> = new Map();
+    private readonly _tasks: Map<string, Task> = new Map();
 
     setTask(name: string, period: any, action: () => void) {
         this._tasks.set(name, {period, action});
