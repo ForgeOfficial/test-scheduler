8e150311a769f3566146e4d101a3173eb1783631
Maxime <maxime.caville@next-u.fr>
2026-01-12 14:35:13 +0100

    feat(red): impl periodicity

--- test/scheduler.spec.ts
+++ test/scheduler.spec.ts
@@ -1,4 +1,5 @@
 import {Scheduler} from "../src/Scheduler";
+import {Always, Never} from "../src/periods/Periodicity";
 
 describe(Scheduler.name, () => {
     it('should can be instantiated', () => {
@@ -9,24 +10,24 @@ describe(Scheduler.name, () => {
     describe('setTask', () => {
         it('should add task', () => {
             const scheduler = new Scheduler();
-            scheduler.setTask("task1", null, () => {});
+            scheduler.setTask("task1", new Always(), () => {});
             expect(scheduler.tasks.size).toEqual(1);
             expect(scheduler.tasks.has('task1')).toBeTruthy();
-            expect(scheduler.tasks.get('task1')?.period).toEqual(null);
+            expect(scheduler.tasks.get('task1')?.period).toBeInstanceOf(Always);
         });
 
         it('should edit task if name already exist', () => {
             const scheduler = new Scheduler();
-            scheduler.setTask("task1", null, () => {});
-            scheduler.setTask("task1", undefined, () => {});
-            expect(scheduler.tasks.get('task1')?.period).toEqual(undefined);
+            scheduler.setTask("task1", new Always(), () => {});
+            scheduler.setTask("task1", new Never(), () => {});
+            expect(scheduler.tasks.get('task1')?.period).toBeInstanceOf(Never);
         });
     });
 
     describe('deleteTask', () => {
         it('should remove task', () => {
             const scheduler = new Scheduler();
-            scheduler.setTask("task1", null, () => {});
+            scheduler.setTask("task1", new Always(), () => {});
             scheduler.deleteTask('task1');
             expect(scheduler.tasks.has('task1')).toBeFalsy();
         })
@@ -36,7 +37,7 @@ describe(Scheduler.name, () => {
         it('should executes all tasks', () => {
             const scheduler = new Scheduler();
             const mockAction = jest.fn();
-            scheduler.setTask("task1", null, mockAction);
+            scheduler.setTask("task1", new Always(), mockAction);
             scheduler.update();
             expect(mockAction).toHaveBeenCalled();
         });
